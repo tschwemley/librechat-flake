d9fa7bc3211859207201bf10658f6dbfe8c4c8dd
Tyler Schwemley <tjschwem@gmail.com>
2025-06-17 04:30:33 -0400

    add default module

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,13 @@
-{
+let
   librechat = import ./librechat.nix;
   ragApi = import ./rag_api.nix;
+in {
+  inherit librechat ragApi;
+
+  default = {
+    imports = [
+      librechat
+      ragApi
+    ];
+  };
 }
