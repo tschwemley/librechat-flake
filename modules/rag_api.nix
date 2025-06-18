5cf2469abd1b5e81ead4cf4f531fd93911d209f3
Tyler Schwemley <tjschwem@gmail.com>
2025-06-18 02:54:24 -0400

    fix workdir

--- modules/rag_api.nix
+++ modules/rag_api.nix
@@ -87,7 +87,7 @@ in {
         script =
           # sh
           ''
-            cd ${cfg.workDir}/rag
+            cd ${cfg.workDir}
 
             ${exportAll cfg.env}
             ${exportAllCredentials cfg.credentials}
